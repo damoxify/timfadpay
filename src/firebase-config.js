3ac26068c1b84d1eb4ba50627efd43faceb05329
Adedamola Ajasa <azeemajasa@gmail.com>
2023-04-25 17:07:59 +0100

    project initialized

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzmF-nIfszB-ws3rrsYAbcEST-BB-VlfQ",
+  authDomain: "timfadpay.firebaseapp.com",
+  projectId: "timfadpay",
+  storageBucket: "timfadpay.appspot.com",
+  messagingSenderId: "447658743045",
+  appId: "1:447658743045:web:787e44bea07af90efc936c",
+  measurementId: "G-1P949HGNPR"
+};
+
+const app = initializeApp(firebaseConfig);
